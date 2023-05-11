a48ebd2420812d3a018228a9f4cf3bbf0f7ba71b
AlMujaadilahMaulana25 <mujamaulana20@gmail.com>
2023-05-11 12:47:54 +0700

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Al Mujaadiah Maulana</h1>
+    <h1> 213510324</h1>
+    <img src="src/assets/WhatsApp Image 2023-05-11 at 12.03.31.jpeg">
   </div>
 </template>
+<style>
+  h1 {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  
+  .about {
+    text-align: center;
+    margin-top: 20px;
+  }
+  
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    margin-top: 10px;
+  }
+</style>
